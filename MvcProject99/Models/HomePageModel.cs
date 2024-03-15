e2119fccdf7d89297eb557bdcd69c8d0f84327bb
מוחמד ותד < @DESKTOP-LA11KBO>
2024-03-16 00:47:01 +0200

    Add project files.

---
+++ MvcProject99/Models/HomePageModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcProject99.Models
+{
+    public class HomePageModel
+    {
+		[Required(ErrorMessage = "Must to be first name")]
+		[StringLength(20, MinimumLength = 2, ErrorMessage = "first name must be between 2 and 20 letters")]
+		public string FirstName { get; set; }
+		[Required]
+		[RegularExpression("^[\\w-]+(?:\\.[\\w-]+)*@(?:[\\w-]+\\.)+[a-zA-Z]{2,7}$", ErrorMessage = "Must contain @")]
+		public string Email {  get; set; }
+		[Required(ErrorMessage="Try Again")]
+		[RegularExpression("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Invailed")]
+		public string Password { get; set; }
+
+
+
+        
+    }
+}
