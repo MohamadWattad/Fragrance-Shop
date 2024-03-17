eb085c6442fb382ae7d2e6cd90b88b0f99b045a9
MohammedDabbah <mohammeddabbah02@icloud.com>
2024-03-17 21:50:49 +0200

    Adding admin page

---
+++ MvcProject99/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProject99.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
